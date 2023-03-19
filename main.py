f0fdcf6144efb73f8f1a6ccc111d9c324ddeefbf
jhchinchero <jhchinchero@gmail.com>
2023-03-19 09:38:51 -0400

    Navegar entre las distintas secciones como la página principal y contacto -hipervínculos (link)

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
-
-
+	return render_template("index.html", title="index")
+	
+@app.route("/contacto")
+def contacto():
+	return render_template("contacto.html", title="contacto")
